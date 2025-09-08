95496e28555fc15aa5cbcb7947c7d80afe5f185d
Ankit Gupta <132796891+ag05123@users.noreply.github.com>
2025-09-08 16:54:25 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 
 
 const sequelize = new Sequelize('railway', 'root', 'FQfjSrKtRhZPZkidgwMMMavAVObKPKGw', {
-  host: 'mysql://root:FQfjSrKtRhZPZkidgwMMMavAVObKPKGw@trolley.proxy.rlwy.net:18971/railway',
+  host: 'trolley.proxy.rlwy.net',
   dialect: 'mysql'
 });
 
